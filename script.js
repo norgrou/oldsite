a625e250b77f6c0f2f60b954ed3ed2a9d63902b6
f o o k d <56459644+G502045@users.noreply.github.com>
2022-05-17 10:43:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 if(confirm("Normally, Both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.")){
   setTimeout(function(){
     while(1)location.reload(1)
-  }, 1000)
+  }, 69420)
 }
